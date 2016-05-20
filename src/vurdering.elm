dd92283555b8a41447106343729132fc4eabcb92
Søren Rindal Nielsen <sn@skm.dk>
2016-05-20 14:30:09 +0200

    fdg

--- src/vurdering.elm
+++ src/vurdering.elm
@@ -28,6 +28,7 @@ type alias Vurdering =
 
 type alias Bygning =
   { id : Int
+  , navn : String
   , arketype : Arketype -- beboelse / erhverv
   , ejdvaerdi : Int
   , kvm : Int
@@ -87,15 +88,35 @@ view : Model -> Html Msg
 view model = div [style [("height", "100%")
                         , ("display", "flex")
                         , ("border", "5px solid black")
-                        , ("margin", "5px")
-                        , ("flex-direction", "column")]]
-             [ text (toString model)
-             , div [style [("display", "flex")
-                         , ("align-items", "center")
-                         , ("justify-content", "space-around")]]
-                 (List.map (block model.state.selectedBygningId) model.vurdering.bygninger)
-             ]
-                          
+                        , ("margin", "5px")]]
+             [ --text (toString model)
+              div [style [("display", "flex")
+                         , ("align-items", "flex-start")
+                         , ("flex-direction", "column")
+                         , ("justify-content", "flex-start")]]
+                 [div [style [("display", "flex")
+                              ,("flex-direction", "column")
+                              ,("height", "200px")
+                              ,("width", "200px")  
+                             ,("background-color", "yellow")
+                             ]]
+                    [div [] [text ("Beboelse: " ++ (toString (calcv model.vurdering.bygninger Privat)))]
+                    ,div [] [text ("Erhverv: " ++ (toString (calcv model.vurdering.bygninger Erhverv)))]]
+                 ,div [style [ ("padding", "4px"), ("display", "flex"), ("flex-direction", "row"), ("flex-wrap", "wrap"), ("justify-content", "space-around")]] (List.map (block model.state.selectedBygningId) model.vurdering.bygninger)
+             ]]
+
+calcv : List Bygning -> Arketype -> Int
+calcv bygninger arketype =
+  bygninger
+    |> List.filter (\b -> b.arketype == arketype)
+    |> List.map ejdvaerdi
+    |> List.foldr (+) 0 
+
+                    
+ejdvaerdi : Bygning -> Int
+ejdvaerdi bygning =
+  bygning.kvm * bygning.kvmpris
+
 block : Maybe Int -> Bygning-> Html Msg
 block selectedBygningId bygning =
   let
@@ -109,16 +130,24 @@ block selectedBygningId bygning =
   div [ onClick (SelectBygningId bygning.id)
        , style [("display", "flex")
               , ( "background-color", (if bygning.arketype == Privat then "Green" else "Red"))
-              , ("justify-content", "center")
+              , ("margin", "4px")
+               ,("justify-content", "center")
               , ("align-items", "center")
               ,( "cursor" , "move")
               ,( "width" , toPx bygning.kvm)
               ,( "height" , toPx bygning.kvmpris)
               , ("border-radius" , "4px")
-              , ("color" , if isSelected then "white" else "yellow") 
+              , ("color" , if isSelected then "yellow" else "white") 
               ]
       ]
-      [text (toString bygning.ejdvaerdi)]
+      [div [style [("display", "flex")
+                  ,("justify-content", "space-around")
+                  ,("flex-direction", "column")]]
+       [div [] [text bygning.navn]
+       , div [] [text ("Areal: " ++ (toString bygning.kvm))]
+       ,div [] [text ("Kvmpris:" ++ (toString bygning.kvmpris))]
+       ,div [] [text ("EjdVærdi:" ++ (toString (bygning.kvm * bygning.kvmpris)))]
+       ]]
 
 toPx : Int -> String
 toPx n = toString n ++ "px"
@@ -145,8 +174,10 @@ initstate : State
 initstate = { selectedBygningId = Nothing, position = dummyPos }
   
 initvurdering : Vurdering
-initvurdering = {bygninger = [{ arketype = Privat, id = 1, ejdvaerdi = 1000, kvm = 150, vaegt = 0.8, kvmpris = 400}
-                             ,{ arketype = Erhverv, id = 2, ejdvaerdi = 800, kvm = 75, vaegt = 0.4, kvmpris = 300}
+initvurdering = {bygninger = [{ navn = "Etage 0", arketype = Privat, id = 1, ejdvaerdi = 1000, kvm = 250, vaegt = 0.8, kvmpris = 400}
+                               , { navn = "Garage", arketype = Privat, id = 3, ejdvaerdi = 1000, kvm = 120, vaegt = 0.8, kvmpris = 80}
+                                 , { navn = "Etage1", arketype = Erhverv, id = 4, ejdvaerdi = 1000, kvm = 250, vaegt = 0.8, kvmpris = 250}
+                             ,{ navn = "Udhus", arketype = Erhverv, id = 2, ejdvaerdi = 800, kvm = 133, vaegt = 0.4, kvmpris = 150}
                              ] 
                 , adresse = "Nabovej 123" 
                 }
